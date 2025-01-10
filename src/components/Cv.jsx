d2bd1ca47f475b72077bcbec7c2e481eb627df85
Ivan0812 <58553204+Ivan0812@users.noreply.github.com>
2025-01-10 12:26:56 +0100

    Rect Portfolio

--- src/components/Cv.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const  DownloadCv = () => {
-  return (
-   <a className='cv' href="../dist/assets/cv/beverbung_ivan.pdf" download >&#8681;</a>
-  )
-}
-
-export default  DownloadCv;
