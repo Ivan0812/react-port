83c21746deef75a07dff5c318cc56b6668281cde
Ivan0812 <58553204+Ivan0812@users.noreply.github.com>
2025-01-10 12:15:14 +0100

    Initial commit

---
+++ src/components/Cv.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const  DownloadCv = () => {
+  return (
+   <a className='cv' href="../dist/assets/cv/beverbung_ivan.pdf" download >&#8681;</a>
+  )
+}
+
+export default  DownloadCv;
