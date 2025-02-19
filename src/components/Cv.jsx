2c98ff0d599e4cb302b087255f851173272afc11
Ivan0812 <58553204+Ivan0812@users.noreply.github.com>
2025-02-19 14:36:50 +0100

    Update Cv.jsx

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 /* eslint-enable no-unused-vars */
 const  DownloadCv = () => {
   return (
-   <a className='cv' href="/cv/beverbung_ivan.pdf" download >&#8681;</a>
+   <a className='cv' href="/cv/bewerbung_ivan.pdf" download >&#8681;</a>
   )
 }
 
